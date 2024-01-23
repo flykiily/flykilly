dd0236c67d2e7f00e7163fed586b5187a7187aed
flykiily <156996340+flykiily@users.noreply.github.com>
2024-01-23 16:10:13 +0800

    Update PY103.py

--- PY103.py
+++ PY103.py
@@ -1,8 +1,4 @@
-# 请在______处使用一行代码或表达式替换
-#
-# 注意：请不要修改其他已给出代码
-
 import random
-______
-for i in range(______):
-    print(______, end=",")
+random.seed(123)
+for i in range(10):
+    print(random.randint(1,1000), end=",")

2b31d003e26a164ee669d841cfd0401ffc25c1ee
flykiily <156996340+flykiily@users.noreply.github.com>
2024-01-23 16:07:00 +0800

    Add files via upload

--- PY103.py
+++ PY103.py
@@ -1,15 +1,8 @@
-# 请在...处使用一行或多行代码替换
+# 请在______处使用一行代码或表达式替换
 #
 # 注意：请不要修改其他已给出代码
 
-n = eval(input("请输入数量："))
-if n>0 and n<=1:
-    cost=n*160
-elif n<=4:
-    cost=n*160*0.9
-elif n<=9:
-    cost=n*160*0.8
-else:
-    cost=n*160*0.7
-cost=int(cost)
-print("总额为:",cost)
+import random
+______
+for i in range(______):
+    print(______, end=",")

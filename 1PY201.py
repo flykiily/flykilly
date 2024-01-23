f8b4276e3cb04287ae80ada0b30eb091aee1a945
flykiily <156996340+flykiily@users.noreply.github.com>
2024-01-23 16:10:49 +0800

    Add files via upload

---
+++ 1PY201.py
@@ -0,0 +1,14 @@
+# 请在______处使用一行代码或表达式替换
+#
+# 注意：请不要修改其他已给出代码
+
+import turtle
+turtle.right(-30)
+___(1)___
+turtle.right(60)
+turtle.fd(200)
+___(2)___
+turtle.fd(200)
+turtle.right(60)
+turtle.fd(200)
+turtle.right(120)

b76c6d567d54a9c362fb25594288e7f40ef2bca5
flykiily <156996340+flykiily@users.noreply.github.com>
2024-01-24 14:50:31 +0800

    Update 3PY103.py

--- 3PY103.py
+++ 3PY103.py
@@ -1,10 +1,7 @@
-# 请在______处使用一行代码或表达式替换
-#
-# 注意：请不要修改其他已给出代码
 import random
-___(1)___
+random.seed(0)
 s = 0
 for i in range(5):
-    n = random.randint(___(2)___)  # 产生随机数
-    s = ___(3)___  
+    n = random.randint(1,97)  # 产生随机数
+    s = s + n**2
 print(s)

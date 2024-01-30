b8a60f20e9ca98ce7115ed76590a997ad25ee8dd
flykiily <156996340+flykiily@users.noreply.github.com>
2024-01-30 15:27:59 +0800

    Update 5PY103.py

--- 5PY103.py
+++ 5PY103.py
@@ -1,12 +1,8 @@
-#
-# 编写代码替换横线
-#
-
 import random
-__________(1)_______________
+n=eval(input())
 random.seed(100)
 for i in range(1,11):
     if i<10:
-        print(___________（2）_________)
+        print(random.randint(1,n),end=",")
     else:
         print(random.randint(1,n))

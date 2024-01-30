257a1615993afac632c9b041682029c277eb0c2a
flykiily <156996340+flykiily@users.noreply.github.com>
2024-01-30 15:05:43 +0800

    Update 5PY101.py

--- 5PY101.py
+++ 5PY101.py
@@ -1,10 +1,6 @@
-#
-# 编写代码替换横线
-#
-
 n = eval(input("请输入一个整数："))
 for i in range(1,n):
     for j in range(1,n):
-        if ______(1)_____________:
-            ______(2)___________
+        if j>=i:
+            print(j,end="")
     print()

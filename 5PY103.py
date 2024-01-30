90435b9cf0e3d5ed35e2c34102ca555bd3b59810
flykiily <156996340+flykiily@users.noreply.github.com>
2024-01-30 15:27:23 +0800

    Add files via upload

---
+++ 5PY103.py
@@ -0,0 +1,12 @@
+#
+# 编写代码替换横线
+#
+
+import random
+__________(1)_______________
+random.seed(100)
+for i in range(1,11):
+    if i<10:
+        print(___________（2）_________)
+    else:
+        print(random.randint(1,n))

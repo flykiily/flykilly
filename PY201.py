bd8ccb7708af6a164898082426edfb3deee074d4
flykiily <156996340+flykiily@users.noreply.github.com>
2024-01-30 15:37:35 +0800

    Update PY201.py

--- PY201.py
+++ PY201.py
@@ -1,15 +1,11 @@
-﻿#
-# 在____________上补充代码
-#
-
 import turtle as t
 color = ['red','green','blue']
 rs = [10,30,60]
 
-for i in range(___(1)___):
+for i in range(3):
     t.penup()
-    t.goto(0, ___(2)___)
-    t. ___(3)___
-    t.pencolor(___(4)___)
-    t.circle(___(5)___)
+    t.goto(0, -rs[i])
+    t. pd()
+    t.pencolor(color[i])
+    t.circle(rs[i])
 t.done()

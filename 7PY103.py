ad113d40744cff7389a539c8fe95ce917c19e420
flykiily <156996340+flykiily@users.noreply.github.com>
2024-01-30 15:42:19 +0800

    Add files via upload

---
+++ 7PY103.py
@@ -0,0 +1,20 @@
+#
+# 在____________上补充代码
+#
+
+
+import random
+
+s = input("请输入随机种子: ")
+ls = []
+for i in range(26):
+    ls.append(chr(ord('a')+i))
+for i in range(10):
+    ls.append(chr(____________))
+    
+random.seed(____________)
+for i in range(10):
+    for j in range(8):
+        print(____________,end='')
+    print()
+

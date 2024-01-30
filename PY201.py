8df6bfe352e062c03d0a96630b4217ea501b5c75
flykiily <156996340+flykiily@users.noreply.github.com>
2024-01-30 15:36:51 +0800

    Add files via upload

---
+++ PY201.py
@@ -0,0 +1,15 @@
+﻿#
+# 在____________上补充代码
+#
+
+import turtle as t
+color = ['red','green','blue']
+rs = [10,30,60]
+
+for i in range(___(1)___):
+    t.penup()
+    t.goto(0, ___(2)___)
+    t. ___(3)___
+    t.pencolor(___(4)___)
+    t.circle(___(5)___)
+t.done()

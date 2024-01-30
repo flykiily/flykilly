053a0451531c57a2a098ef1f4fab3e0e16422fb8
flykiily <156996340+flykiily@users.noreply.github.com>
2024-01-30 15:32:35 +0800

    Update 6PY102.py

--- 6PY102.py
+++ 6PY102.py
@@ -1,19 +1,15 @@
-#
-# 在____________上补充代码
-#
-
 import random
 
 random.seed(25)
-n = ___________(1)____________
+n = random.randint(1,100)
 for m in range(1,7):
     x = eval(input("请输入猜测数字："))
     if x == n:
         print("恭喜你，猜对了！")
         break
-    elif  ___________(2)___________:
+    elif  x>n:
         print("大了，再试试")
     else:
         print("小了，再试试")
-    if ___________(3)___________:
+    if m == 6
         print("谢谢！请休息后再猜")
